43f1a215502323dddc7d85b814b217507eb66a36
743218 <lifre5647@stu.pender.k12.nc.us>
2026-09-09 11:52:49 -0400

    I'm lazy ok

--- Wolfer/Client/Managers/HookManager/Hooks/World/Entity/ActorOnPushHook.h
+++ Wolfer/Client/Managers/HookManager/Hooks/World/Entity/ActorOnPushHook.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "../../FuncHook.h"
+
+class ActorOnPushHook : public FuncHook {
+private:
+	using func_t = bool(__thiscall*)(Actor*);
+	static inline func_t oFunc;
+
+	static bool ActorOnPushHookCallback(Actor* _this) {
+    return false; //LEL
+		return oFunc(_this);
+	}
+public:
+	ActorOnPushHook() {
+		OriginFunc = (void*)&oFunc;
+		func = (void*)&ActorOnPushHookCallback;
+	}
+};
