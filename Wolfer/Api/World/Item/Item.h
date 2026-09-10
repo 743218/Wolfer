b483eadfa1cdbe0981c91bbd91218649ed2b1955
743218 <lifre5647@stu.pender.k12.nc.us>
2026-09-10 11:59:56 -0400

    Update Item.h

--- Wolfer/Api/World/Item/Item.h
+++ Wolfer/Api/World/Item/Item.h
@@ -27,6 +27,82 @@ public:
 		return Memory::CallVFunc<18, bool>(this);
 	}
 
+	bool isDye() {
+		return Memory::CallVFunc<15, bool>(this);
+	}
+
+	bool isPattern() {
+		return Memory::CallVFunc<39, bool>(this);
+	}
+
+	bool canBeDepleted() {
+		return Memory::CallVFunc<30, bool>(this);
+	}
+
+	bool isDyeable() {
+		return Memory::CallVFunc<14, bool>(this);
+	}
+
+	bool isDamagable() {
+		return Memory::CallVFunc<13, bool>(this);
+	}
+
+	bool isCandle() {
+		return Memory::CallVFunc<12, bool>(this);
+	}
+
+	bool isBlockPlanterItem() {
+		return Memory::CallVFunc<10, bool>(this);
+	}
+
+	bool isBucket() {
+		return Memory::CallVFunc<11, bool>(this);
+	}
+
+	bool isHumanoidArmor() {
+		return Memory::CallVFunc<9, bool>(this);
+	}
+
+	bool isMusicDisc() {
+		return Memory::CallVFunc<6, bool>(this);
+	}
+
+	bool isActorPlacerItem() {
+		return Memory::CallVFunc<60, bool>(this);
+	}
+
+	int getEnchantValue() {
+		return Memory::CallVFunc<51, int>(this);
+	}
+
+	int getEnchantSlot() {
+		return Memory::CallVFunc<50, int>(this);
+	}
+
+	int getDamageChange(int unbreaking) {
+		return Memory::CallVFunc<52, int, int>(this, unbreaking);
+	}
+
+	void hurtActor(class ItemStack& item, class Actor& actor, class Mob& attacker) {
+		return Memory::CallVFunc<78, void, class ItemStack, class Actor, class Mob>(this, item, actor, attacker);
+	}
+
+	void hitActor(class ItemStack& item, class Actor& actor, class Mob& attacker) {
+		return Memory::CallVFunc<79, void, class ItemStack, class Actor, class Mob>(this, item, actor, attacker);
+	}
+
+	bool isHandEquipped() {
+		return Memory::CallVFunc<37, bool>(this);
+	}
+
+	bool canDestroyInCreative() {
+		return Memory::CallVFunc<43, bool>(this);
+	}
+
+	bool isLiquidClipItem() {
+		return Memory::CallVFunc<45, bool>(this);
+	}
+
 	bool isThrowable() {
 		return Memory::CallVFunc<19, bool>(this);
 	}
@@ -63,6 +139,43 @@ public:
 		return nameContains("_axe");
 	}
 
+	bool isNotStackableOverOne() { //wow arent i lazy
+		if (nameContains("potion")) return true;
+		if (nameContains("_sword")) return true;
+				if (nameContains("_axe")) return true;
+				if (nameContains("_pickaxe")) return true;
+				if (nameContains("_hoe")) return true;
+				if (nameContains("_shovel")) return true;
+				if (nameContains("_helmet")) return true;
+				if (nameContains("_chestplate")) return true;
+				if (nameContains("_leggings")) return true;
+				if (nameContains("_boots")) return true;
+				if (nameContains("elytra")) return true;
+				if (nameContains("trident")) return true;
+				if (nameContains("shears")) return true;
+				if (nameContains("flint_and_steel")) return true;
+				if (nameContains("rod")) return true;
+				if (nameContains("carrot_on_a_stick")) return true;
+				if (nameContains("compass")) return true;
+		return false;
+	}
+
+	bool isCrystal() {
+		return nameContains("end_crystal");
+	}
+
+	bool isTrident() {
+		return nameContains("trident");
+	}
+
+	bool isHoe() {
+		return nameContains("_hoe");
+	}
+
+	bool isShovel() {
+		return nameContains("_shovel");
+	}
+
 	bool isBoomBox() {
 		return nameContains("boombox");
 	}
@@ -97,6 +210,8 @@ public:
 		if (nameContains("chainmail")) return 2;
 		if (nameContains("iron")) return 3;
 		if (nameContains("diamond")) return 4;
+		if (nameContains("netherite")) return 5;
+		if (nameContains("turtle")) return 3;
 		return 0;
 	}
 
@@ -106,6 +221,7 @@ public:
 		if (nameContains("stone")) return 2;
 		if (nameContains("iron")) return 3;
 		if (nameContains("diamond")) return 4;
+		if (nameContains("netherite")) return 5;
 		return 0;
 	}
 };
